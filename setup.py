b4bc287f52d875a0b536187f1847eb4f7d3ae873
alexseuf <48165001+alexseuf@users.noreply.github.com>
2026-09-13 12:43:05 +0200

    Bump plugin version to 0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="cbpi4-sdm630",
-    version="0.1.0",
+    version="0.1.1",
     description="CraftBeerPi4 SDM630 Modbus RTU power sensor plugin",
     author="Alexander Seufert",
     include_package_data=True,
