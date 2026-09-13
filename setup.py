ac53f535f1aaa8f6d4c9608637ad67ef2d6d6070
alexseuf <48165001+alexseuf@users.noreply.github.com>
2026-09-13 12:16:35 +0200

    Add plugin package setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="cbpi4-sdm630",
+    version="0.1.0",
+    description="CraftBeerPi4 SDM630 Modbus RTU power sensor plugin",
+    author="Alexander Seufert",
+    include_package_data=True,
+    packages=["cbpi4-sdm630"],
+    package_data={"cbpi4-sdm630": ["*"]},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["minimalmodbus>=2.1.1", "pyserial>=3.5"],
+)
